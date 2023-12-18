765ff74587dcd5609b8157067c7b56b31a0af083
Afamilo <nichomartins@gmail.com>
2023-12-18 00:42:37 -0600

    added some changes

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 let myLibrary = [];
 
-function Book(title, author, page, read) {
+function Book(title, author, page, isRead) {
     this.title = title;
     this.author = author;
     this.page = page;
-    this.read = read;
+    this.isRead = isRead;
 }
-const newBook = new Book(title, author, pages, reads)
 
 function addBook() {
-  return  myLibrary.push(newBook); 
+   let title = document.querySelector('#title').value
+   let author = document.querySelector('#author').value
+   let page = document.querySelector('#page').value
+   let isRead = document.querySelector('#isRead').checked
+   let newBook = new Book(title, author, page, isRead)
+  myLibrary.push(newBook); 
+  toRender();
 }
 
-const bookGrid = document.querySelector('.book-grid');
-let newBookBtn = document.querySelector('.addBook-btn');
-let title = document.querySelector('#title').value;
-let author = document.querySelector('#author').value;
-let pages = document.querySelector('#pages').value;
-let reads = document.querySelector('#isReads').value;
+  const container = document.querySelector('.container');
+  const addBookbtn = document.querySelector('.addBook-btn')
+  const submit = document.querySelector('#submit');
 
-function toRender() {
-  const books = document.querySelectorAll('.books');
-  books.forEach(function(book){
-    for (let i = 0; i < myLibrary.length; i++) {
-      createBook(myLibrary[i]);
-    }
-  })
-}
+addBookbtn.addEventListener('click', function(){
+  container.classList.add('active') 
+})
+
 function createBook(item) {
+    const bookGrid = document.querySelector('#books-grid');
     const bookDiv = document.createElement('div');
     const titleDiv = document.createElement('div');
     const authorDiv = document.createElement('div');
     const pageDiv = document.createElement('div');
-    const readBtn = document.createElement('button');
+    const isReadBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
     
     bookDiv.classList.add('book');
-    bookDiv.setAttribute('id', myLibraryindexof(item));
-
-    titleDiv.textContent = item.tittle
+    bookDiv.setAttribute('id', myLibrary.indexOf(item));
+    
+    titleDiv.textContent = item.title
     titleDiv.classList.add('title');
     bookDiv.appendChild(titleDiv);
 
@@ -49,7 +49,75 @@ function createBook(item) {
     pageDiv.classList.add('page');
     bookDiv.appendChild(pageDiv)
  
-    readBtn.classList.add('button');
+    isReadBtn.classList.add('isReadBtn');
+    isReadBtn.style.fontSize = '20px'
+    isReadBtn.addEventListener('click', function(){
+      item.isRead = !item.isRead;
+      setData();
+      toRender();
+    })
+
+    bookDiv.appendChild(isReadBtn);
     
-    
+    if(item.isRead === false) {
+      isReadBtn.textContent = "Not Read";
+      isReadBtn.style.backgroundColor = 'red'
+    }else {
+      isReadBtn.textContent = 'Read';
+      isReadBtn.style.backgroundColor = 'green';
+    }
+
+    removeBtn.textContent = 'Remove';
+    removeBtn.classList.add('removeBtn')
+    removeBtn.setAttribute('id', 'removeBtn');
+    bookDiv.appendChild(removeBtn);
+
+    bookGrid.appendChild(bookDiv);
+
+    removeBtn.addEventListener('click', function(){
+        removeBook();
+        setData();
+    })
 }
+
+function toRender() {
+  const display = document.querySelector('.books-grid');
+  const books = document.querySelectorAll('.book')
+  books.forEach(function(book){
+       display.removeChild(book);
+  })
+    for (let i = 0; i < myLibrary.length; i++) {
+      createBook(myLibrary[i]);
+    }
+ 
+}
+
+submit.addEventListener('click', function(event){
+  event.preventDefault();
+  addBook();
+  setData();
+  clearform();
+  container.classList.remove('active')
+  })
+
+function clearform() {
+  title.value = '';
+  author.value = '';
+  page.value = '';
+  isRead.value = '';
+}
+
+function setData() {
+  localStorage.setItem(`myLibrary`, JSON.stringify(myLibrary));
+}
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  toRender();
+}
+
+function restore() {
+   myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+   toRender();
+}
+
+restore();
